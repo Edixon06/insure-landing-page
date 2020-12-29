878bb0d576a444402b364fe7aa993e4603149505
Edixon <edixon.serrano06@gmail.com>
2020-12-29 15:20:05 -0400

    Menu tipo hamburguesa para movil realizado

---
+++ app.js
@@ -0,0 +1,13 @@
+let hamburguer = document.getElementById('hambButton');
+let menu = document.getElementById('navMenu');
+let click = 0;
+hamburguer.addEventListener('click', function(){
+   
+    if (click === 0) {
+        menu.className = 'display-menu';
+        click = 1;
+    } else if(click === 1){
+        menu.className = 'hidden-menu';
+        click = 0;
+    }
+})
